2f15b3a9e1d514eac30f7ff3588097601b078ea3
Gustavo Quenupe Lobo <gustavoqe.75@gmail.com>
2023-09-08 04:48:11 -0300

    Correção PSR-4 - Worfklows

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,12 +9,12 @@ import './bootstrap';
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 // import 'vendor/livewire/livewire/dist/livewire.esm.js';
 
-import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import Clipboard from '@ryangjchandler/alpine-clipboard'
+// import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+// import Clipboard from '@ryangjchandler/alpine-clipboard'
  
-Alpine.plugin(Clipboard)
+// Alpine.plugin(Clipboard)
  
-Livewire.start()
+// Livewire.start()
 
 import.meta.glob([
     '../images/**',
